b3346ad157c1be8169bb2c15c70cf4d36d1520c7
Igor Antony <igor.prohmann@gmail.com>
2019-09-20 21:44:48 -0300

    Criação das Entidades
    Entidade Tipo
    Entidade Conta

---
+++ codificação/financeiro/src/main/java/br/com/codar/financeiro/entity/Conta.java
@@ -0,0 +1,81 @@
+package br.com.codar.financeiro.entity;
+
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Conta {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne
+	private Tipo tipo;
+	
+	@Column(name = "descricao", nullable = false)
+	private String descricao;
+	
+	private Date dataVencimento;
+	
+	private Date dataPagamento;
+	
+	private Double valor;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Tipo getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(Tipo tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+
+	public Date getDataPagamento() {
+		return dataPagamento;
+	}
+
+	public void setDataPagamento(Date dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	
+	
+}
